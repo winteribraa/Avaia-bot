7f0e5af53a470ccc396a9984cb99e397070eb792
winteribraa <realmsniga@gmail.com>
2026-05-13 22:22:08 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "https://discord.gg/avaia1 satup by ibra-winter",
         // Activity type number (0 = Playing).
         type: 0, 
       },
@@ -136,7 +136,7 @@ export const botConfig = {
     },
     footer: {
       // Default footer text used in bot embeds.
-      text: "Titan Bot",
+      text: "avaia bot",
       // Footer icon URL (null = no icon).
       icon: null,
     },
