dff5bf34906ce3c2934a2348cda221a4ed05f173
winteribraa <realmsniga@gmail.com>
2026-05-14 15:14:44 +0300

    Update ticket.js

--- src/commands/Ticket/ticket.js
+++ src/commands/Ticket/ticket.js
@@ -163,7 +163,7 @@ description: panelMessage,
                     .setCustomId("create_ticket")
 .setLabel(buttonLabel)
                     .setStyle(ButtonStyle.Primary)
-                    .setEmoji("<a:ac_kikilove:1492575215681536127>"),
+                    .setEmoji(":ac_kikilove:"),
             );
 
             try {
